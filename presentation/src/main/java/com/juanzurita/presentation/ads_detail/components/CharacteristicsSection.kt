843b4c03593ba19ce8e9acbf2819d7a5bf1647dd
Juan Zurita <juanzurita@besoccer.com>
2025-02-22 14:36:47 +0100

    refactor: remove unused imports

--- presentation/src/main/java/com/juanzurita/presentation/ads_detail/components/CharacteristicsSection.kt
+++ presentation/src/main/java/com/juanzurita/presentation/ads_detail/components/CharacteristicsSection.kt
@@ -3,7 +3,6 @@ package com.juanzurita.presentation.ads_detail.components
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.juanzurita.presentation.ads_detail.ui.theme.Dimens
